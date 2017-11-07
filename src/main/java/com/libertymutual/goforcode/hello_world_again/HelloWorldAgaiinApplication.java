09a49b1a669bf442154c0f232228354c45b2d8ad
Karen Alyea <karen.alyea@libertymutual.com>
2017-11-07 14:09:12 -0500

    Initial commit of the potato survey application.

---
+++ src/main/java/com/libertymutual/goforcode/hello_world_again/HelloWorldAgaiinApplication.java
@@ -0,0 +1,12 @@
+package com.libertymutual.goforcode.hello_world_again;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldAgaiinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldAgaiinApplication.class, args);
+	}
+}
